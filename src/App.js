deab29248f0f68f364df5331818d6dcbe378763d
kratkikh <akratkikh.exceedteam@gmail.com>
2022-03-07 11:49:13 +0300

    commit 3

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learning redux!!
+          Learning redux!!!
         </p>
         
       </header>
